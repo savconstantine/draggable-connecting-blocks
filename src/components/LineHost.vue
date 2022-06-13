eb46c094f1865210f1110c61fa650aed89da85d2
Konstantin Savusia <savconstantine@gmail.com>
2022-06-13 20:00:24 +0600

    linehost component

---
+++ src/components/LineHost.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="host" :class="hostDirect + 'Host'" :ref="hostDirect + 'Host' + blockIndex" @click="setLine( blockIndex, hostDirect + 'Host')"></div>
+</template>
+
+<script>
+    export default {
+        name: 'LineHost',
+        props: {
+            hostDirect: {
+                type: String,
+                default: 'top'
+            },
+            blockIndex: {
+                type: Number,
+                default: 0
+            },
+        }
+    }
+</script>
